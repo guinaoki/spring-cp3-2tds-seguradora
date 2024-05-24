0503936a1f5ff824cc1c045671e6173b0c23d88e
guinaoki <rm551456@fiap.com.br>
2024-05-23 21:13:02 -0300

    cp finalizado

--- src/main/java/br/com/fiap/seguradora/resource/ResourceDTO.java
+++ src/main/java/br/com/fiap/seguradora/resource/ResourceDTO.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 
 public interface ResourceDTO<Request, Response> {
 
-    //Também deve fazer o findAll() com Example
 
     ResponseEntity<Response> findById(Long id);
 
